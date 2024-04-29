504e06249be0b4ac41dc926515215476a63c3113
elias.safo <elias.safo@bonial.com>
2024-04-29 14:40:08 +0200

    created Env file

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,15 @@ FROM python:3.11
 
 WORKDIR /Elias
 
-COPY requirements.txt .
+ENV POETRY_VERSION=1.7.1 \
+    POETRY_HOME="/opt/poetry" \
+    POETRY_NO_INTERACTION=1 \
+    POETRY_VIRTUALENVS_CREATE=false
 
-RUN pip install -r requirements.txt
+ENV PATH="$POETRY_HOME/bin:$PATH"
+RUN curl -sSL https://install.python-poetry.org | python3 -
+
+RUN poetry install --no-root
 
 COPY src .
 
--- src/database.py
+++ src/database.py
@@ -2,16 +2,19 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DB_HOST = os.environ.get("POSTGRES_HOST", "database")
-DB_USER = os.environ.get("POSTGRES_USER", "elias")
-DB_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "secret_123")
-DB_PORT = os.environ.get("POSTGRES_PORT", "5432")
-DB_NAME = os.environ.get("POSTGRES_DB", "post_db")
+load_dotenv()
+
+DB_HOST = os.getenv("POSTGRES_HOST")
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DB_PORT = os.getenv("POSTGRES_PORT")
+DB_NAME = os.getenv("POSTGRES_DB")
 
 # Construct SQLAlchemy database URL
 SQLALCHEMY_DATABASE_URL = (
-    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
